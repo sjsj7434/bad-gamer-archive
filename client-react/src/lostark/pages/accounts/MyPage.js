0b71523b163195ee0e3e8939e1634ffca6e2ef80
L33 <sjsj7434@naver.com>
2023-04-19 22:22:48 +0900

    Create MyPage.js

---
+++ client-react/src/lostark/pages/accounts/MyPage.js
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+
+const MyPage = (props) => {
+	return (
+		<Container style={{maxWidth: "600px"}}>
+			MINE
+		</Container>
+	);
+}
+
+export default MyPage;
