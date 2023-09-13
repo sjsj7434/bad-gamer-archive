cdc761127dda672832c61a5cdaeb78dbaab09c5e
sjsj7434 <57528375+sjsj7434@users.noreply.github.com>
2023-09-13 16:30:38 +0900

    개발을 위해 엄격 모드 해제
    2번 렌더링으로 혼란스러워서 엄격 모드 해제

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // index.js에서 StrictMode 존재하면 두번 랜더링, 개발 모드에서만 적용됩니다. 생명주기 메서드들은 프로덕션 모드에서 이중으로 호출되지 않습니다.
+// 진짜 두번 렌더링되고 있는지, 개발모드라서 두번 렌더링되는지 혼란스러움 그래서 개발 끝마치면 엄격 모드 켜서 확인할 예정
 root.render(
-	<React.StrictMode>
+	// <React.StrictMode>
 		<App />
-	</React.StrictMode>
+	// </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
