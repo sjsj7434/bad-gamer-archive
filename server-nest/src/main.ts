72a4f73eca02edd7de4bc27a73e1d468aed22033
sjsj7434 <sjsj7434@naver.com>
2023-11-30 14:56:09 +0900

    config 위치 수정 & https 설정 주석 추가

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -3,9 +3,22 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	let httpsOptions = null; //https를 사용하려면 이 코드를 주석 처리
+
+	// httpsOptions = {
+	// 	key: fs.readFileSync('src/private-key.pem'),
+	// 	cert: fs.readFileSync('src/public-certificate.pem'),
+	// 	passphrase: "test",
+	// 	// openssl 인증서 생성 : req -x509 -newkey rsa:4096 -keyout private-key.pem -out public-certificate.pem -days 3650
+	// 	// openssl 인증서 passphrase 제거 : rsa -in private-key.pem -out private-key2.pem
+	// };
+
+	const app = await NestFactory.create(AppModule, {
+		httpsOptions,
+	});
 
 	app.use(cookieParser());
 
@@ -14,7 +27,7 @@ async function bootstrap() {
 
 	app.enableCors({
 		origin: true,
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS'],
 		credentials: true,
 	});
 
@@ -32,7 +45,7 @@ async function bootstrap() {
 
 	await app.listen(serverConfig.port);
 
-	console.log(`[main.ts] Server is running on http://localhost:${serverConfig.port}`);
+	console.log(`[main.ts] Server is running on ${httpsOptions === null ? "http" : "https"}://localhost:${serverConfig.port}`);
 }
 
 bootstrap();
