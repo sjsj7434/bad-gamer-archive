fc6663cf8892f6c648bceb9591855795fc6561d6
sjsj7434 <sjsj7434@naver.com>
2023-12-21 16:29:55 +0900

    Update MyPage.js

--- client-react/src/common/pages/account/MyPage.js
+++ client-react/src/common/pages/account/MyPage.js
@@ -222,7 +222,7 @@ const MyPage = () => {
 							{
 								accountData.profilePictureURL !== null ? 
 								<>
-									<OverlayTrigger trigger="click" placement="right-start" overlay={userPopover} rootClose={true}>
+									<OverlayTrigger trigger="click" placement="right-end" overlay={userPopover} rootClose={true}>
 										<Image src={accountData.profilePictureURL} roundedCircle className="profilePicture" />
 									</OverlayTrigger>
 								</>
@@ -298,7 +298,7 @@ const MyPage = () => {
 												<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
 											</svg>
 										</div>
-										
+
 										<div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
 											<Button onClick={() => { navigate("activate/lostark") }} variant="outline-success" style={{ width: "100%", fontSize: "1.2rem" }}>인증하기</Button>
 										</div>
