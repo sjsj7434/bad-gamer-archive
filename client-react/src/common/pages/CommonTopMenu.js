29ff0ac202887ffef42f75fe23d5cf48a7b4585f
sjsj7434 <sjsj7434@naver.com>
2023-12-21 10:50:32 +0900

    버튼 이미지 변경

--- client-react/src/common/pages/CommonTopMenu.js
+++ client-react/src/common/pages/CommonTopMenu.js
@@ -310,7 +310,7 @@ const CommonTopMenu = (props) => {
 						{
 							props.accountData.status === "login" ?
 							<>
-								<div onClick={() => { logoutAccount() }} style={{ display: "flex", flexDirection: "row", alignItems: "center", marginRight: "0.3rem" }}>
+								<div title="로그아웃" onClick={() => { logoutAccount() }} style={{ display: "flex", flexDirection: "row", alignItems: "center", marginRight: "0.3rem" }}>
 									<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#9b9d9e" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
 										<path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
 										<path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
@@ -319,17 +319,17 @@ const CommonTopMenu = (props) => {
 							</>
 							:
 							<>
-								<Nav.Link active={props.currentMenu === "/account/login" ? true : false} onClick={() => menuClick("/account/login")}>
+								<Nav.Link title="로그인" active={props.currentMenu === "/account/login" ? true : false} onClick={() => menuClick("/account/login")}>
 									<div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginRight: "0.3rem" }}>
-										<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#9b9d9e" className="bi bi-person-square" viewBox="0 0 16 16">
-											<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-											<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+										<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-box-arrow-in-up-right" viewBox="0 0 16 16">
+											<path fillRule="evenodd" d="M6.364 13.5a.5.5 0 0 0 .5.5H13.5a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 13.5 1h-10A1.5 1.5 0 0 0 2 2.5v6.636a.5.5 0 1 0 1 0V2.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6.864a.5.5 0 0 0-.5.5z"/>
+											<path fillRule="evenodd" d="M11 5.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793l-8.147 8.146a.5.5 0 0 0 .708.708L10 6.707V10.5a.5.5 0 0 0 1 0v-5z"/>
 										</svg>
 									</div>
 								</Nav.Link>
 							</>
 						}
-						&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;
 
 						<Navbar.Toggle onClick={() => setShowOffcanvas(true)} style={{ padding: "1px 6px", border: "0px" }} />
 					</Nav>
